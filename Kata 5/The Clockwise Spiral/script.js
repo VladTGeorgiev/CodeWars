5a19d49eeca35bf9e29d1411b336befaaca8489a
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-20 00:02:27 +0100

    Solution to The clockwise spiral

--- Kata 5/The Clockwise Spiral/script.js
+++ Kata 5/The Clockwise Spiral/script.js
@@ -1,3 +1,45 @@
-function createSpiral(N) {
+function createSpiral (N) {
+	if (Number.isInteger(N)) {
+        let result = new Array(N).fill().map(() => new Array(N).fill('')); // create empty n x n array
+		let counter = 1;
+		let startCol = 0;
+		let endCol = N - 1;
+		let startRow = 0;
+		let endRow = N - 1;
+		while (startCol <= endCol && startRow <= endRow) {
+			for (let i = startCol; i <= endCol; i++) {
+				result[startRow][i] = counter;
+				counter++;
+			}
+			startRow++;
+			for (let i = startRow; i <= endRow; i++) {
+				result[i][endCol] = counter;
+				counter++;
+			}
 
+			endCol--;
+
+			for (let i = endCol; i >= startCol; i--) {
+				result[endRow][i] = counter;
+				counter++;
+			}
+
+			endRow--;
+			for (let i = endRow; i >= startRow; i--) {
+				result[i][startCol] = counter;
+				counter++;
+			}
+
+			startCol++;
+		}
+		return result;
+	} else {
+		return (result = []);
+	}
 }
+
+createSpiral(3);
+
+createSpiral(4);
+
+createSpiral(5);
